5f21f64f66838ff4c824fd6a35f8ba59915a6b61
OwerHub <owerwild@gmail.com>
2021-05-09 18:06:50 +0200

    Start the PizzaHead

--- frontend/src/components/Head.jsx
+++ frontend/src/components/Head.jsx
@@ -1,21 +1,50 @@
 import React, { useState, useEffect } from "react";
+import Conatcticon from "../img/contact-3018.svg";
 
 let headers = ["Üdvözlünk", "Rólunk", "Pizzáink", "Asztalfoglalás"];
 
 function PageHead(props) {
+  const [isContact, setContact] = useState(true);
+
+  let contactStyle = {};
+
+  if (isContact) {
+    contactStyle = { transform: "translateY(7vh) " };
+  } else {
+    contactStyle = { transform: "translateY(-3vh)" };
+  }
+
   let bannerStyle = {
-    transform: `translateX(-${(props.number - 1) * 250}px)`,
+    transform: `translateX(-${(props.number - 1) * 250}px) `,
   };
+
+  function hideContact() {
+    console.log("kontakt");
+    setContact(!isContact);
+  }
 
   return (
     <div className="mainCont">
-      <div className="mainHead">
-        <div className="headCenter">
-          <div className="scrollBanner" style={bannerStyle}>
-            {headers.map((data, iterator) => (
-              <div key={iterator}>{data} </div>
-            ))}
+      <div className="mainNameWrapper">
+        <div className="mainHead">
+          <div className="contactButton" onClick={() => hideContact()}>
+            <img src={Conatcticon} alt="React Logo" height="30px" />
           </div>
+          <div className="headCenter">
+            <div className="scrollBanner" style={bannerStyle}>
+              {headers.map((data, iterator) => (
+                <div key={iterator}>{data} </div>
+              ))}
+            </div>
+          </div>
+          <div className="contactButton" style={{ color: "black" }}>
+            kap
+          </div>
+        </div>
+        <div className="contactsHead" style={contactStyle}>
+          <span>Cím: Budapest, Rutyakötyöge 4</span>
+          <span>Telefonszám: 555-45-54</span>
+          <span>mail: sdaw3dwad@gmail.com</span>
         </div>
       </div>
     </div>
