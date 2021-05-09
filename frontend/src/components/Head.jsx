ef9068a053839adfbd580cdb7fa0b650aa703172
OwerHub <owerwild@gmail.com>
2021-05-09 03:17:28 +0200

    Műűűűköööödiiiiik!

--- frontend/src/components/Head.jsx
+++ frontend/src/components/Head.jsx
@@ -1,10 +1,22 @@
 import React, { useState, useEffect } from "react";
 
+let headers = ["Üdvözlünk", "Rólunk", "Pizzáink", "Asztalfoglalás"];
+
 function PageHead(props) {
+  let bannerStyle = {
+    transform: `translateX(-${(props.number - 1) * 10}vw)`,
+  };
+
   return (
     <div className="mainCont">
       <div className="mainHead">
-        <div className="headCenter">pagenr {props.number}</div>
+        <div className="headCenter">
+          <div className="scrollBanner" style={bannerStyle}>
+            {headers.map((data, iterator) => (
+              <div key={iterator}>{data} </div>
+            ))}
+          </div>
+        </div>
       </div>
     </div>
   );
